ce37b8db907d897f04fa54ca5434df6b4a32bfc0
alumne_1er <sgallardo@institutmvm.cat>
2021-01-25 11:47:14 +0100

    Nueva entrada

--- ex2.py
+++ ex2.py
@@ -1,15 +1,18 @@
 def main():
-    n1 = int(input("Introdueix un numero:"))
-    n2 = int(input("Introdueix un altre numero:"))
-    
-    s = max(n1, n2)
-    g = min(n1, n2)
-    
-    while g!=0:
-        op = g
-        g = s%g
-        s = op
-    print("EL MAXIM COMU DIVISOR ENTRE {0} i {1} es: {2}".format(n1, n2, op))
+    u = 0
+    num = []
+    x = 10
+    while (u < x):
+        z = int(input('Introdueix un numero:'))
+        num.append(z)
+        u += 1
+
+    def calc(num, x):
+        w = sum(num)
+        v = w / x
+        return (v)
+
+    print('Mitjana = ', calc(num, x))
     
 if __name__ == "__main__":
     main()
