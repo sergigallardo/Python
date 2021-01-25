2b1283a0a3e37396f9d583a74b4c2f4d96b8085e
alumne_1er <sgallardo@institutmvm.cat>
2021-01-25 11:58:29 +0100

    Actualizacion de Ex7.py

--- Ex7.py
+++ Ex7.py
@@ -2,8 +2,12 @@ def main():
    n1 = int(input("Introdueix un numero:"))
    n2 = int(input("Introdueix un numero:"))
    n3 = int(input("Introdueix un numero:"))
-
-
+   if n1==n2==n3:
+       r= ((n1+n2+n3)*3)
+       print(r)
+   else:
+       r=(n1+n2+n3)
+       print(r)
 
 if __name__ == "__main__":
     main()
