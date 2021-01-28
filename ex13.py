2f51c068277490e6ed1bfc52dc8be72d17770836
alumne_1er <sgallardo@institutmvm.cat>
2021-01-28 11:51:24 +0100

    update ex13.py

--- ex13.py
+++ ex13.py
@@ -2,7 +2,7 @@ def main():
     val1 = int(input("Introdueix un valor per val1:"))#Input de val1
     val2 = int(input("Introdueix un valor per val2:"))#Input de val2
 
-    print("val1=%d  val2=%d" %(val1,val2)) #Mostra els valors introduits.
+    print("val1=%d  val2=%d" %(val1,val2)) #Mostra els valors introduits
     op = val1
     val1 = val2
     val2 = op
