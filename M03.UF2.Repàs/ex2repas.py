48b0b350ca4c0facd35d1c752522badeca98ac64
sergigallardo <60615701+sergigallardo@users.noreply.github.com>
2021-03-24 18:55:55 +0100

    Add files via upload

---
+++ M03.UF2.Repàs/ex2repas.py
@@ -0,0 +1,20 @@
+def llista():
+    num = []
+    x = 1
+
+    while x < 146:
+        if x % 2 == 0:
+            y = x / 5
+            num.append(y)
+        else:
+            y = x * 2
+            num.append(y)
+        x = x + 1
+    return num
+
+def main():
+
+    print(llista())
+
+if __name__ == '__main__':
+   main()
