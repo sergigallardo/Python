a2c5e718ad0901942f4685180f9689f9762ac3e1
sergigallardo <60615701+sergigallardo@users.noreply.github.com>
2021-03-25 12:08:35 +0100

    Update ex2repas.py

--- M03.UF2.Repàs/ex2repas.py
+++ M03.UF2.Repàs/ex2repas.py
@@ -1,20 +1,27 @@
 def llista():
-    num = []
+   num = []
+   x = 1
+
+   while x < 146:
+       if x % 2 == 0:
+           y = x // 5
+           num.append(y)
+       else:
+           y = x * 2
+           num.append(y)
+       x = x + 1
+   return num
+
+def llista2():
+    num2 = []
     x = 1
-
     while x < 146:
-        if x % 2 == 0:
-            y = x // 5
-            num.append(y)
-        else:
-            y = x * 2
-            num.append(y)
+        num2.append(x)
         x = x + 1
-    return num
-
+    return num2
 def main():
-
-    print(llista())
+   print(llista2())
+   print(llista())
 
 if __name__ == '__main__':
-   main()
+  main()
